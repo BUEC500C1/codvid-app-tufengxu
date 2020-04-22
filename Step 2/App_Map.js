faa265387354fa76812f3e7b8cc4ead3937ef9d0
Fengxu Tu <55207104+tufengxu@users.noreply.github.com>
2020-04-22 12:38:55 -0400

    Update and rename App.js to App_Map.js

--- Step 2/App_Map.js
+++ Step 2/App_Map.js
@@ -2,9 +2,7 @@ import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import React from 'react';
 import {StyleSheet, Text, View, TextInput, Platform} from 'react-native';
 
-export default
-
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <MapView

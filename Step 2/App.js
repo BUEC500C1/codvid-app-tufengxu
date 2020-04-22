556bd411385440a8c1f4ce546fd555259b70d0e4
Fengxu Tu <55207104+tufengxu@users.noreply.github.com>
2020-04-22 12:35:49 -0400

    Create App.js

---
+++ Step 2/App.js
@@ -0,0 +1,22 @@
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import React from 'react';
+import {StyleSheet, Text, View, TextInput, Platform} from 'react-native';
+
+export default
+
+class App extends React.Component {
+  render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+        latitude: 42.3505,
+        longitude: -71.1054
+        }}
+      />
+      
+    );
+  }
+}
